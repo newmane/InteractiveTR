03d067853b3782fdbd5433658f5c7a823918cb2f
Emily Newman <emily-jayne1@live.com>
2017-03-28 20:29:03 -0400

    add captions to pictures

--- book.js
+++ book.js
@@ -1,8 +1,9 @@
+var number = 2;
 $(document).ready(function () {
-	$("#getbook").click(function() {
+	$("#openbook").click(function() {
 		$.ajax({
 			dataType: "html",
-			url: "http://tarheelreader.org/2015/03/30/at-the-zoo-10/4",
+			url: "http://tarheelreader.org/2015/03/30/at-the-zoo-10/" + number,
 			crossDomain: true,
 			error: function(xhr, textStatus, error){
       			console.log(xhr.statusText);
@@ -10,10 +11,21 @@ $(document).ready(function () {
       			console.log(error);
   			},
   			success: function(data) {
-  				console.log("SUCCESS");
-  				var imgurl = $(data).find('img.thr-pic').attr("src");
-  				imgurl = "http://tarheelreader.org/" + imgurl;
-  				document.getElementById('img').src=imgurl;
+  				if(number <= 12) {
+	  				number++;
+	  				console.log("SUCCESS");
+	  				var imgurl = $(data).find('img.thr-pic').attr("src");
+	  				imgurl = "http://tarheelreader.org/" + imgurl;
+	  				document.getElementById('img').src=imgurl;
+	  				var text = $(data).find('p.thr-caption').text();
+	  				document.getElementById('text').innerHTML=text;
+	  				$("#openbook").addClass("next");
+	  				document.getElementById('openbook').innerHTML="next";
+	  			}
+	  			else {
+
+	  			}	
+
   			}
 		})
 	});
