1016ad2bf4d869b27552c3d0d2ad9efad456c02d
Emily Newman <emily-jayne1@live.com>
2017-04-06 21:22:14 -0400

    add audio and back button

--- book.js
+++ book.js
@@ -1,33 +1,55 @@
 var number = 2;
 $(document).ready(function () {
-	$("#openbook").click(function() {
-		$.ajax({
-			dataType: "html",
-			url: "http://tarheelreader.org/2015/03/30/at-the-zoo-10/" + number,
-			crossDomain: true,
-			error: function(xhr, textStatus, error){
-      			console.log(xhr.statusText);
-      			console.log(textStatus);
-      			console.log(error);
-  			},
-  			success: function(data) {
-  				if(number <= 12) {
-	  				number++;
-	  				console.log("SUCCESS");
-	  				var imgurl = $(data).find('img.thr-pic').attr("src");
-	  				imgurl = "http://tarheelreader.org/" + imgurl;
-	  				document.getElementById('img').src=imgurl;
-	  				var text = $(data).find('p.thr-caption').text();
-	  				document.getElementById('text').innerHTML=text;
-	  				$("#openbook").addClass("next");
-	  				document.getElementById('openbook').innerHTML="next";
-	  			}
-	  			else {
-	  				document.getElementById('container').innerHTML= "The End";
-
-	  			}	
-
-  			}
-		})
+	$("#openbook").click( function() {
+		getData();
+	});
+	$('#back').click(function() {
+		number = number - 2;
+		number
+		getData();
 	});
 });
+function animalNoise(text) {
+	var hasGorilla = text.includes("Gorilla");
+	var hasTiger = text.includes("Tiger");
+	if(hasGorilla == true) {
+		document.getElementById('audio').innerHTML = "<audio controls autoplay><source src='Gorilla.mp3' type='audio/mpeg'></audio>";
+	} else if(hasTiger == true) {
+		document.getElementById('audio').innerHTML = "<audio controls autoplay><source src='Tiger.mp3' type='audio/mpeg'></audio>";
+	}
+}
+function getData() {
+	$.ajax({
+		dataType: "html",
+		url: "http://tarheelreader.org/2015/03/30/at-the-zoo-10/" + number,
+		crossDomain: true,
+		error: function(xhr, textStatus, error){
+      		console.log(xhr.statusText);
+      		console.log(textStatus);
+      		console.log(error);
+  		},
+  		success: function(data) {
+  			if(number <= 12) {
+	  			number++;
+	  			console.log("SUCCESS");
+	  			if(number == 3) {
+	  				$("#back").removeClass("hidden");
+	  			}
+	  			document.getElementById('audio').innerHTML = "";
+	  			var imgurl = $(data).find('img.thr-pic').attr("src");
+	  			imgurl = "http://tarheelreader.org/" + imgurl;
+	  			document.getElementById('img').src=imgurl;
+	  			var text = $(data).find('p.thr-caption').text();
+	  			document.getElementById('text').innerHTML=text;
+	  			animalNoise(text);
+	  			$("#openbook").addClass("next");
+	  			document.getElementById('openbook').innerHTML="next";
+	  		}
+	  		else {
+	  			document.getElementById('container').innerHTML= "The End";
+
+	  		}	
+
+		}
+	})
+}
